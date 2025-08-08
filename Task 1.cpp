7653191b3b7c93a954ca1ab935defaa87d320156
omarrashraff <ashrafmora2005@gmail.com>
2025-08-08 15:04:28 +0300

    Add files via upload

---
+++ Task 1.cpp
@@ -0,0 +1,266 @@
+/// NAME: omar ashraf mohamed    ID:20235021///
+/// NAME: Ganna mohamed          ID:20236024///
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class PrintJob
+{
+  public:
+  string pcname;
+  int arrinmins;
+  string arrivaltime;
+  int duration;
+// Default constructor
+  PrintJob()
+  {
+    pcname = " ";
+    arrivaltime = " ";
+    arrinmins = 0;
+    duration = 0;
+  }
+// Parameterized constructor
+  PrintJob(string pc, string aim, int dur)
+  {
+    pcname = pc;
+    arrinmins = convertintomins(aim);
+    duration = dur;
+    arrivaltime = aim;
+  }
+// Convert the arrival time from string to minutes
+  static int convertintomins (string time)
+  {
+    if(time.length()<5 || time[2] != ':') { return -1; }
+    int hrs = stoi(time.substr(0,2));
+    int mins = stoi(time.substr(3,2));
+    return (hrs*60) + mins;
+  }
+  static bool createjfromstr(string data , PrintJob &job)
+  {
+    // Find the positions of the spaces in the line
+    size_t space1 = data.find(' ');
+    size_t space2 = data.find(' ', space1+1);
+    if(space1 == string::npos || space2 == string::npos){return false;}
+// Extract PC name, time, and duration using substr
+    string pcname = data.substr(0, space1);    // PC name is before the first space
+    string time = data.substr(space1 + 1, space2 - space1 - 1);    // Arrival time is between the first and second spaces
+    string duration = data.substr(space2 + 1);    // Duration is after the second space
+    int mins = convertintomins(time);
+    if(mins==-1) return false;
+
+    int dur = stoi(duration);    // Converted duration from string into integer
+
+    job=PrintJob(pcname, time, dur);
+    return true;
+  }
+};
+
+class Node
+{
+public:
+    PrintJob pjob;
+    Node * next;
+// Parameterized constructor
+    Node(PrintJob job)
+    {
+        pjob = job;
+        next=nullptr;
+    }
+};
+
+class PriorityQueue
+{
+    public:
+    Node *head;
+    int counter;
+    int totocctime;      // Tracks total printer occupation time
+    int totwait;        // Tracks total waiting time
+// Default constructor
+    PriorityQueue()
+    {
+        head = nullptr;
+        counter = 0;
+        totocctime = 0;
+        totwait = 0;
+    }
+
+// To add the printing job into the printing queue
+      void enqueue(PrintJob job) {
+// To create a new node
+        Node* newn = new Node(job);
+        Node* current = head;
+        Node* prev = nullptr;
+
+        while (current != nullptr &&
+              (current->pjob.arrinmins < job.arrinmins ||
+              (current->pjob.arrinmins == job.arrinmins &&
+               current->pjob.duration <= job.duration))) {
+            prev = current;
+            current = current->next;
+        }
+// Insert at the begining
+        if (prev == nullptr) {
+            newn->next = head;
+            head = newn;
+        }
+// Insert in the middle or the end
+        else {
+            prev->next = newn;
+            newn->next = current;
+        }
+
+        counter++;
+    }
+
+    // Convert the execution time from minutes to string (hh:mm format)
+    string converttimeback(int mins)
+    {
+        int hours = mins /60;     // Extracted the hours
+        int m = mins % 60;     // Used the modulus operator to get the remaining minutes after extracting the hours
+        string hh;     // a string to store the coverted hours in two digits
+        string mm;     // a string to store the coverted minutes two digits
+// Storing the hours in hh format
+// If "hours" is less than 10 add 0 to the tens
+        if(hours<10)
+        {
+            hh = "0" + to_string(hours);
+        }
+// Storing the hours in mm format
+// If "hours" is larger than 10 convert it into string as it is
+        else
+        {
+          hh = to_string(hours);
+        }
+        // If "m" is less than 10 add 0 to the tens
+        if(m<10)
+        {
+            mm = "0" + to_string(m);
+        }
+        // If "m" is larger than 10 convert it into strung as it is
+        else
+        {
+          mm = to_string(m);
+        }
+      return hh + ":" + mm;
+    }
+// To print the details of the printing job
+    void print()
+    {
+        if(head==nullptr){
+            cout<<"No jobs in the queue." << endl;
+        }
+
+        cout << "Jobs Executed: " << counter << endl;
+// Declared "currtime" to track when the printer starts executing each job
+        int currtime;
+// In case the queue isn't empty therefore the "currtime" starts from when the head executes
+        if(head)
+        {
+            currtime = head -> pjob.arrinmins;
+        }
+// In case the queue is empty therefore the "currtime" is set to 0
+        else
+        {
+            currtime = 0;
+        }
+
+        Node * current = head;
+        Node *prev = nullptr;
+// If queue isn't empty, set "currtime" to the arrival time of the first job
+        if (current)
+        {
+            currtime=current->pjob.arrinmins;
+        }
+// If queue is empty, set "currtime" to 0
+        else
+        {
+            currtime=0;
+        }
+        totocctime = 0;
+        totwait = 0;
+// Traverses the queue
+        while(current !=nullptr)
+        {
+// Removes the current printing job from the queue
+// If the queue is empty update the head to the next node (implemented in the dequeue function above)
+           if(prev == nullptr)
+           {
+            head = current -> next;
+           }
+// If the queue isn't empty; link the previous node to the next node, bypassing the current one
+           else
+           {
+            prev -> next = current -> next;
+           }
+
+// Declared "wait" to calculate the time a job waits in the queue before it starts executing
+           PrintJob job=current->pjob;
+           int wait;
+           if (currtime < job.arrinmins )
+           {
+                currtime = job.arrinmins;
+           }
+           wait=currtime-job.arrinmins;
+           totwait += wait;
+           totocctime += job.duration;
+//Print job details
+           cout<<"PC: " << job.pcname << "  Arrival " <<converttimeback(job.arrinmins)<<"   Duration: " << job.duration << "   executed at: " << converttimeback(currtime) << "   Waiting time: " << wait << endl;
+// Update current time
+           currtime +=  job.duration;
+           current=current->next;
+        }
+        cout << endl;
+// Prints the printer's total occupation time
+        cout << "The printer was occupied for: "  << totocctime << " minutes" << endl;
+        cout << endl;
+// Prints the average waiting time
+        cout << "Average waiting time for all jobs is ";
+        if (counter > 0)
+        {
+           cout << float(totwait)/counter;
+        }
+        else
+        {
+            cout << 0;
+        }
+        cout << " minutes" << endl;
+    }
+
+
+};
+int main()
+{
+    PriorityQueue pq;
+// Create a file named "printer.txt"
+    ifstream file ("printer.txt");
+    string line;
+// Checks if file is open
+    if(!file.is_open())
+    {
+      cout << "Error! Can't open the file";
+      return 1;
+    }
+    getline(file, line);
+// Reads each line from the file and copies into a PriorityQueue object "job"
+    while(getline(file, line))
+    {
+        PrintJob job;
+        if(PrintJob::createjfromstr(line,job)) {
+                pq.enqueue(job);
+        }else
+// Error handling in case there were invalid lines
+        {
+            cout<< "skipping the invalid lines: " << line << endl;
+        }
+
+    }
+// Closes the file
+    file.close();
+// Prints the file's content
+    pq.print();
+
+    return 0 ;
+}
---
+++ task2.cpp
@@ -0,0 +1,149 @@
+/// NAME: omar ashraf mohamed    ID:20235021///
+/// NAME: Ganna mohamed          ID:20236024///
+
+#include <iostream>
+#include <cstring>
+#include <fstream>
+using namespace std;
+
+//Class Node for the linked list
+class Node
+{
+public:
+    string info;    // info for the linked list
+    Node*next;      // the location of the linked list
+    Node(string in):info(in),next(nullptr){}  // constructor
+};
+
+// class stack implementation for the linked list
+class Stack
+{
+private:
+    Node*topofthestack;  // element that appear in the top of the stack
+public:
+    Stack(){topofthestack=nullptr;}  //constructor
+
+    // function used to push an item to the stack
+    void push(string item)
+    {
+        Node*n=new Node(item);  // initialize an value
+        if(n==nullptr)   // check if the stack is empty or not
+        {
+            cout << "stack push can not allocate memory" << endl;
+        }
+        else
+        {
+            n->info=item;
+            n->next=topofthestack;
+            topofthestack=n;
+        }
+    }
+
+    // check if the stack is empty or not
+    bool isEmpty() {return topofthestack==nullptr;}
+
+    // function used to pop an item to the stack
+    string pop()
+    {
+        if(isEmpty()) {  // check if is empty or not
+                cout << "stack is empty" << endl;
+                return "";
+        }
+        else
+        {
+            Node*temp=topofthestack;
+            string value = temp->info;
+            topofthestack=topofthestack->next;
+            temp=temp->next=nullptr;
+            delete temp;  // delete the current item
+            return value; // return the current item after delete it
+        }
+    }
+
+
+    // clear the stack
+    void Clear()
+    {
+         while(!isEmpty()) {pop();}
+    }
+};
+
+
+
+int main()
+{
+    Stack Backward , Forward , temp ;  // declare variable for backward and forward and temp navigation
+    string URLs[9] ;     // declare an array to store urls
+    int i =0;
+
+    ifstream file ("URLs.txt");    // declare a file input stream to read from the file
+    if(!file){
+        cout<<" Error in the opening of the file" << endl;   // error message if file can not open
+        return 1;   // exit the program with an error
+    }
+
+    while(i<9 && getline(file,URLs[i])){ i++; } // read up to 9 urls from the file and store them in the urls array
+
+    for(int j=0;j<9;j++){  // push the urls array into the temp
+        temp.push(URLs[j]);
+    }
+
+    while(!temp.isEmpty()) { Backward.push(temp.pop()); }  // transfer every url from temp into the backward navigation stack
+                                                           // pop from temp and push to backward
+    string Currentpage = Backward.pop(); // pop the first url from backward and name it as current page
+
+    // read the 2nd line from the file which contain backward and forward
+    string line;
+    getline(file,line);
+    file.close();
+
+    // tokenize the command using strtok function to split by space
+    char *cmd = strtok(&line[0]," ");  // get first command
+    string commnd[10];  // array to store the command
+    int cmdcounter=0;  // counter for the array
+    while(cmd !=nullptr && cmdcounter<10){  // loop through the command line and split it into individual commands
+        commnd[cmdcounter++]=cmd;   // store each command in the array
+        cmd=strtok(nullptr," ");    // get the next command
+    }
+
+    // the process of each 10 commands
+    for(int j=0;j<10;j++)
+    {
+        cout<<"\nChoice:" << (j+1) << " [" << commnd[j] << "]\n";  // display the current command choice
+
+        if(commnd[j]=="Backward" || commnd[j]=="backward"){  // handle the backward command
+            if(!Backward.isEmpty()){
+                Forward.push(Currentpage);  // if the backward is not empty push current page into the forward
+                Currentpage=Backward.pop();// pop the top of the url from backward and set it as current page
+            }
+        }else if(commnd[j]=="Forward" || commnd[j]=="forward" ){  // handle the forward command
+                if(!Forward.isEmpty()){
+                Backward.push(Currentpage);   // if the forward is not empty push current page into the backward
+                Currentpage=Forward.pop();  // pop the top of the url from forward and set it as current page
+                }
+        }else {
+                cout << "Invalid command: " << commnd[j] << " — Skipping.\n";  // for handling incorrect command
+        }
+
+        cout << " === The current page is : ===\n " << Currentpage << endl;  // display the current page
+
+        string next="";
+        if(!Forward.isEmpty()){
+            next=Forward.pop();   // pop the next page from forward
+            Forward.push(next);    // push it back
+        }
+        cout<< "=== The next one is : ===\n" <<(next.empty()? "NONE" : next)<<endl;  // display the next page
+
+        string previous="";
+        if(!Backward.isEmpty()){
+            previous=Backward.pop(); // pop the previous page from backward
+            Backward.push(previous); // push it back
+        }
+        cout<< "=== The previous one is : ===\n" <<(previous.empty()? "NONE" : previous)<<endl; // display the previous page
+
+        cout << "-----------------------------------------------------------------------------------------------"<<endl;
+    }
+
+
+    return 0;
+}
